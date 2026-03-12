0ca2f8dbf2ea8483b6120ee1b15d0ee90876566b
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-03-12 15:34:34 -0300

    colors

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 function Colors(){
+
+    const [selectedColor, setSelectedColor]= useState('blue')
     const colors =[
         {id:'blue', name:'Titânio Azul', image:'/img/iphone-blue.jpg', colorClass:'bg-blue-500'},
         {id:'silver', name:'Titânio Natural', image:'/img/iphone-gray...jpg', colorClass:'bg-gray-300'},
@@ -21,9 +23,22 @@ function Colors(){
                     <p className="text-xl text-gray-400">Quatro acabamentos em titânio </p>
                 </div>
             </div>
+            <div className="flex justify-center mb-12">
+                <div className="relative w-full max-w-3xl">
+                    <div className="relative flex items-center justify-center min-h[500px]">
+                        <img src={colors.find(color =>color.id === selectedColor).image} alt="iphone 17" className="max-w-full max-h-[600px] mx-auto0" />
+                    </div>
+
+                    <div>
+                        <div>
+                            <h3>{colors.find(color=> color.id === selectedColor).name}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </section>
-    )
+    
 }
 
 export default Colors
