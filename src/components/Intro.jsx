733df6b8448ef21eeb89506de4eadd2898da300a
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-03-12 11:29:41 -0300

    finalização Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,15 +11,25 @@ function Intro(){
         <section className="bg-black py-20 px-6">
             <div className="max-w-5xl mx-auto text-center">
                 <h1 className="text-6xl md:text-8xl font-bold mb-6 ">iPhone 17 Pro</h1>
-                <p className="text-2xl md:text-4xl " >Titâno. Tão Forte. Tão leve. Tão Pro.</p>
-                <p>O desing mais refinado que já criamos. Titânio de grau auroespacial. Chip A18 Pro. Sistema de câmera Pro revolucionário.</p>
-                <div>
-                    <button>Compre Agora</button>
-                    <button>Saiba Mais</button>
+                <p className="text-2xl md:text-4xl text-gradient font-bold" >Titâno. Tão Forte. Tão leve. Tão Pro.</p>
+                <p className="text-lg md:text-xl text-gray-300 mb-10 max-w-3xl mx-auto">O desing mais refinado que já criamos. Titânio de grau auroespacial. Chip A18 Pro. Sistema de câmera Pro revolucionário.</p>
+                <div className="flex flex-col md:flex-row gap-4 justify-center items-center mb-16">
+                    <button className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-full text-lh font-medium transition-all duration-300 hover:scale-105 shadow-lg shadow-blue-500/50 cursor-pointer">Compre Agora</button>
+                    <button className="border-2 border-white rounded-full text-white hover:bg-white hover:text-black px-8 py-4 text-lg font-medium transition-all duration-300 hover:scale-105 cursor-pointer">Saiba Mais</button>
                 </div>
             </div>
+            <div className="grid grid-cols-2 md:grid-col-4 gap-6 mx-auto max-w-5xl">
+            {
+                specs.map((spec, i)=>(
+                    <div key={i} className=" cursor-pointer bg-gray-900 rounded-2xl p-6 hover:bg-gray-800 transition-all duration-300 hover:scale-105">
+                        <p className={`text-3xl font-bold ${spec.color}`}>{spec.value}</p>
+                        <p>{spec.label}</p>
+                    </div>
+                ))
+            }
+            </div>
         </section>
-    )4
+    )
 }
 
 export default Intro;
